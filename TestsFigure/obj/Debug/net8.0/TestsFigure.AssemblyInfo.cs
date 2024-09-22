9f2885d514197f892576d99b0e48983c86bde0c2
Luclpor <luclpor1337@gmail.com>
2024-09-22 15:54:43 +0300

    delete folder

--- TestsFigure/obj/Debug/net8.0/TestsFigure.AssemblyInfo.cs
+++ TestsFigure/obj/Debug/net8.0/TestsFigure.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestsFigure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8f80da1992f5dd98d0700ea3f42080b2a586ad2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c85cd3b19d70897d857941f7bc6ddea5fdb600db")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestsFigure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestsFigure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
